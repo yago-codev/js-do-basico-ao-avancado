a562a0879491112d2d26356767c9d72234e118ae
yago-codev <yagomilano92@gmail.com>
2020-04-22 19:42:08 -0300

    Objetos: verificando as chaves de um objeto

---
+++ 08-verificando-as-chaves-de-objetos/index.js
@@ -0,0 +1,8 @@
+let obj = {
+  chave1: 1,
+  chave2: 2,
+  chave3: 3,
+};
+
+// verificando todas as chaves presentes no objeto:
+console.log(Object.keys(obj)); // OBS: o método "Object.keys()" irá retornar um array.
