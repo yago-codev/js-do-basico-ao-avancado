0e95652592192fb0dd79a566b17eef14fc81837e
yago-codev <yagomilano92@gmail.com>
2020-04-22 19:04:09 -0300

    Arrays: o que são propriedades

---
+++ 03-o-que-sao-propriedades/index.js
@@ -0,0 +1,10 @@
+let nome = 'João';
+
+// verificando quantos 'caracteres' a string possui:
+// 
+// notação ponto:
+console.log(nome.length); // 4
+//
+//
+// notação colchetes:
+console.log(nome['length']);
