c23029e668c9a67baa4c3bd446efe993198d05d8
yago-codev <yagomilano92@gmail.com>
2020-04-22 21:30:22 -0300

    Arrays: iterando/percorrendo arrays

---
+++ 09-loop-em-arrays/index.js
@@ -0,0 +1,6 @@
+const nomes = ['Zé', 'Godofredo', 'João', 'Matheus', 'Paulo'];
+
+// iterando/percorrendo o array
+for (let i = 0; i < nomes.length; i++) {
+  console.log(`Elemento: ${nomes[i]}, índice: ${i}`);
+}
